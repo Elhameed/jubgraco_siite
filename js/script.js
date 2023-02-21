e5afe2eb5475a020e507d7ab5775751835cae588
Elhameed <teniolaajani2004@gmail.com>
2023-02-22 00:12:50 +0200

    added thiings

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Swiper Slider
+var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
